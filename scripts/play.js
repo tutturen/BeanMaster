ccd8c847f0f325160a1017bd5bbff50b0ef77ce5
Thor Even Tutturen <t.tutturen@gmail.com>
2015-10-13 17:08:32 +0200

    Change script to use new names

--- scripts/play.js
+++ scripts/play.js
@@ -3,8 +3,8 @@ var request = require('request');
 var fs = require('fs');
 
 var API_URL = 'http://drachten.informatik.uni-mannheim.de/api';
-var PLAYER_NAME = 'Larry';
-var PLAYER2_NAME = 'Moe';
+var OLD_NAME = 'Good-Old-Larry';
+var NEW_NAME = 'Shiny-New-Moe';
 
 var oldPath = '../out/OldBeanMaster.jar';
 var jarPath = '../out/BeanMaster.jar';
@@ -12,23 +12,24 @@ var jarPath = '../out/BeanMaster.jar';
 function spawnJavaProcs(gameString) {
 	if (!fs.existsSync(oldPath)) {
 		oldPath = jarPath;
+    OLD_NAME = NEW_NAME + '-Clone';
 	}
 
-	var child1 = spawn('java', ['-jar', oldPath, gameString, PLAYER_NAME, '0'], {
+	var child1 = spawn('java', ['-jar', oldPath, gameString, OLD_NAME, '0'], {
 		stdio: 'inherit'
 	});
 
-	var child2 = spawn('java', ['-jar', jarPath,  gameString, PLAYER2_NAME, '6'], {
+	var child2 = spawn('java', ['-jar', jarPath,  gameString, NEW_NAME, '6'], {
 		stdio: 'inherit'
 	});
 
 	return 0;
 }
 
-request(API_URL + '/creategame/' + PLAYER_NAME, function(error, response, body) {
+request(API_URL + '/creategame/' + OLD_NAME, function(error, response, body) {
 	var gameID = body;
 	if(!error && response.statusCode == 200) {
-		request(API_URL + '/joingame/' + body + '/' + PLAYER2_NAME, function(error, response, body) {
+		request(API_URL + '/joingame/' + body + '/' + NEW_NAME, function(error, response, body) {
 			if(!error && response.statusCode == 200 && body === '1') {
 				var retCode = spawnJavaProcs(gameID);
 				if (retCode === 0) {
